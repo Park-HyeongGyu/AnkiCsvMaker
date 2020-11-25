edb1d2ed78cd3d6fcce6496fd301cda410817495
LinuxServer <yum_ki@yumki-private.asia-northeast3-a.c.compact-record-296323.internal>
2020-11-25 11:55:36 +0000

    Linux+Tag

--- Eng_Anki_ForLinuxTag.py
+++ Eng_Anki_ForLinuxTag.py
@@ -1,4 +1,5 @@
 import csv
+from Eng_Anki_CSV_ForTag import list_maker, pprint, HowMakeTag
 
 class CSV_manual:
     def __init__(self, file_name, list_original):
@@ -10,8 +11,7 @@ class CSV_manual:
         self.file_name = file_name
         self.list_original = list_original'''
     def CSV_writer(self):
-        name_csv = "D:\CSV_For_Anki\Eng_started_200130\ " + self.file_name + ".csv"
-        name_csv = name_csv.replace(" ", "")
+        name_csv = "/home/yum_ki/anki_csv_files/" + self.file_name + ".csv"
         #print("this is name_csv" + name_csv)
         with open(name_csv, 'w', encoding = 'utf-8', newline='') as f:
             wr = csv.writer(f)
@@ -25,50 +25,14 @@ class CSV_manual:
 #End of the class CSV_manual
 #To distinguish original csv module and new csv class that I made
 #I wrote all the name that contains 'csv' in upper case and set class name as CSV_manual
-    
-                  
-        
-def list_maker():
-    column = int(input("input a column : "))
-    tag = input("input a tag : ")
-    maker_list = []
-    line = 0
-    print("if you input 'end' then the input proceds end")
-    while True:
-        count = 0
-        maker_list.append([])
-        #print("appending line......")
-        while count < column:
-            tem = input(str(line+1) + ": ")
-            if tem == "end":
-                del maker_list[line]
-                return maker_list
-            maker_list[line].append(tem)
-            #print("appending tem.... this is count" + str(count) + " this is line" + str(line))
-            count += 1
-        #main_list.append([])
-        maker_list[line].append(tag)
-        line +=1
-        print()
-#End of the maker
-
-def pprint(list):
-    for line in list:
-        print(line)
-#End of the pprint
-
-def HowMakeTag():
-    print("<모의고사 태그 붙이는법>")
-    print("%Y%MH%G -> %G:학년")
-    print("Ex) 2020년 6월 고3모의고사:20063")
-
 
 #From here, int main()
 def main():
     print("제작자:박형규")
     print("제작일:2020년4월30일")
+    print("리눅스 포매팅:2020년 11월 25일")
     print("입력받은 텍스트들을 csv파일로 만들어주는 프로그램.")
-    print("저장 경로:D:\CSV_For_Anki\Eng_started_200130")
+    print("저장 경로: /home/yum_ki/anki_csv_files/")
     print("위의 폴더 안에 자동으로 .csv가 붙은 파일을 만들어줍니다.")
     HowMakeTag()
     main_list = list_maker()
