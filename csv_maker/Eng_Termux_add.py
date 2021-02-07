1d8c6a3e6b3bbce8c8a620ea22dd0573e546403d
Park-HyeongGyu <phg7889@gmail.com>
2021-02-07 20:17:53 +0900

    termux interface

--- csv_maker/Eng_Termux_add.py
+++ csv_maker/Eng_Termux_add.py
@@ -46,6 +46,7 @@ def list_maker():
         content_list.append(tag)
         CSV_module.CSV_adder(content_list)
         line += 1
+        print()
 #End of the maker
 
 def pprint(list):
@@ -59,7 +60,7 @@ def main():
     print("제작일:2020년4월30일")
     print("예문 모듈 제작일:2020년 11월 29일")
     print("append 수정일:2021년2월1일")
-    print("Linux Version.")
+    print("Linux-termux Version.")
     print("입력받은 텍스트들을 csv파일로 만들어주는 프로그램.")
     print("저장 경로:현재경로")
     print("위의 폴더 안에 자동으로 .csv가 붙은 파일을 만들어줍니다.")
