27bb29c8ab53005031c4aacabed209d8be0c34b0
Park-HyeongGyu <phg7889@gmail.com>
2021-02-19 17:57:59 +0900

    Modifying structures_2021-0219

--- csv_maker/Eng_Termux_add.py
+++ csv_maker/Eng_Termux_add.py
@@ -1,58 +1,10 @@
-import csv
-from Eng_Anki_CSV_ForTag import HowMakeTag
+from how_make_tag import HowMakeTag
+from list_maker_add import list_maker
 
-class CSV_manual:
-    def __init__(self, file_name):
-        #print("생성자 호출!")
-        self.d_file_name = "/data/data/com.termux/files/home/csvs/Eng/"+file_name + ".csv"
-    '''def set_info(self, file_name, list_original):
-        print("set_info")
-        self.file_name = file_name
-        self.list_original = list_original'''
-    def CSV_adder(self, content):
-        with open(self.d_file_name, 'a+', encoding = 'utf-8', newline='') as f:
-            wr = csv.writer(f)
-            wr.writerow(content)
-#End of the class CSV_manual
-#To distinguish original csv module and new csv class that I made
-#I wrote all the name that contains 'csv' in upper case and set class name as CSV_manual
-    
-                  
-        
-def list_maker():
-    column = int(input("input a column : "))
-    tag = input("input a tag : ")
-    line = 1
-    file_name = input("input file name : ")
-    CSV_module = CSV_manual(file_name)
-    print("if you input 'end' then the input proceds end")
-    while True:
-        count = 0
-        #print("appending line......")
-        content_list = []
-        while count <= column:
-            if count==column:#In case of example sentence.
-                tem = input("Example of "+str(line)+": ")
-                if tem == "end":
-                    return 0 
-                content_list.append(tem)
-            else:
-                tem = input(str(line) + ": ")
-                if tem == "end":
-                    return 0
-                content_list.append(tem)
-            #print("appending tem.... this is count" + str(count) + " this is line" + str(line))
-            count += 1
-        content_list.append(tag)
-        CSV_module.CSV_adder(content_list)
-        line += 1
-        print()
-#End of the maker
-
-def pprint(list):
-    for line in list:
-        print(line)
-#End of the pprint
+def GetFilename():
+    a = input("Input file name : ")
+    to_reutrn = "/data/data/com.termux/files/home/csvs/Eng/" + a + ".csv"
+    return to_reutrn
 
 #From here, int main()
 def main():
@@ -65,7 +17,7 @@ def main():
     print("저장 경로:현재경로")
     print("위의 폴더 안에 자동으로 .csv가 붙은 파일을 만들어줍니다.")
     HowMakeTag()
-    list_maker()
+    list_maker(GetFilename())
     print("Press enter to exit program......", end="")
     input()
 
---
+++ csv_maker/how_make_tag.py
@@ -0,0 +1,12 @@
+def HowMakeTag():
+    print("<태그 붙이는법>")
+    print("----모의고사----")
+    print("%Y%MH%G -> %G:학년")
+    print("Ex) 2020년 6월 고3모의고사: 20063")
+    print("----문제집----")
+    print("수특:st%Y%T -> %T:영어=>E, 영어독해연습=>EP")
+    print("만약 문제 번호를 추가하고 싶다면 st%Y%T%P%N -> %P:page, %N:number")
+    print("Ex) 2021년 수능 대비 수능특강 영어 : st21E")
+    print("----책----")
+    print("MFI = Why doesn't microfinance work?")
+    print()
